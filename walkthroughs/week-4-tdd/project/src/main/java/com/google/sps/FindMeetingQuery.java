7c582c65b0310bfacd29b3d24fd9dfae75dfe19e
Governess <simps422@umn.edu>
2020-06-28 19:48:58 -0500

    create algorithm for finding available meeting times

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,75 @@
 
 package com.google.sps;
 
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList; 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Collection;
+import java.util.Collections;   
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        ArrayList<TimeRange> openSlots = new ArrayList<TimeRange>();
+        ArrayList<TimeRange> filledTime = new ArrayList<TimeRange>();
+        Set<String> Attendees = new HashSet<String>(request.getAttendees());
+
+        for(Event event: events){
+            Set<String> eventAttendees = new HashSet<String>(event.getAttendees());
+            eventAttendees.retainAll(Attendees);
+            
+            if (!eventAttendees.isEmpty()) {
+                filledTime.add(event.getWhen());
+            }
+        }
+
+        ArrayList<TimeRange> allEventTimes = returnEventTime(filledTime);
+
+        int endTime = TimeRange.START_OF_DAY;
+        long duration = request.getDuration();
+
+        for(TimeRange curEvent: allEventTimes){
+            if (curEvent.start() - endTime >= duration){
+                openSlots.add(TimeRange.fromStartEnd(endTime, curEvent.start(), false));
+            }
+            endTime = curEvent.end();
+        }
+
+        if (TimeRange.END_OF_DAY - endTime >= duration) {
+            openSlots.add(TimeRange.fromStartEnd(endTime, TimeRange.END_OF_DAY, true));
+        }
+
+        return openSlots;
+    }
+
+    private ArrayList<TimeRange> returnEventTime(ArrayList<TimeRange> eventTime){
+        if (eventTime.size() < 1) {
+            return eventTime;
+        }
+
+        Collections.sort(eventTime, TimeRange.ORDER_BY_START);
+
+        ArrayList<TimeRange> allEventTimes = new ArrayList();  
+        int startTime = eventTime.get(0).start();
+        int endTime = eventTime.get(0).end();
+
+        for (int curIndex = 0; curIndex < eventTime.size(); curIndex++){
+            TimeRange curEvent = eventTime.get(curIndex);
+
+            if (TimeRange.fromStartEnd(startTime, endTime, false).overlaps(curEvent)) {
+                endTime = Math.max(endTime, curEvent.end());
+            }
+
+            else {
+                allEventTimes.add(TimeRange.fromStartEnd(startTime, endTime, false));
+                startTime = curEvent.start();
+                endTime = curEvent.end();
+            }
+        }
+
+        allEventTimes.add(TimeRange.fromStartEnd(startTime, endTime, false));
+        
+        return allEventTimes;
+    }
 }
